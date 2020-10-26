ecc66b807ad48d98976f1aa9ab0f46b6089aabc2
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-26 16:34:05 +0530

    First commit

---
+++ src/test/java/testTDD.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testTDD {
+
+    @Test
+    public void given_Mins_dist_ReturnAmount() {
+        CabMethods cabService = new CabMethods();
+        long fare = cabService.fare(2,2);
+        Assert.assertEquals(22,fare);
+    }
+}
